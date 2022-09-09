79829cba1e225d3796c5a727ef9619119219472d
Dup4 <lyuzhi.pan@gmail.com>
2022-09-09 22:01:03 +0800

    refactor: use ThriftFormatter

--- src/format.ts
+++ src/format.ts
@@ -1,43 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { IOptions } from "./types";
+import ThriftFormatter from "./internal/ThriftFormatter";
 
-class StructLine {
-  number = "";
-  paramOptions = "";
-  paramName = "";
-  defaultValue = "";
-  description = "";
-  annotation = "";
-}
-
-class EnumLine {
-  number = "";
-  paramName = "";
-  hasDescription = "";
-  description = "";
-  annotation = "";
-}
-
-export function formatThrift(
-  content: string,
-  options?: IOptions,
-): [string, boolean] {
-  const defaultOptions: IOptions = {};
-  content = deleteExtraEmptyLines(content, options || defaultOptions);
-  return [content, true];
-}
-
-function deleteExtraEmptyLines(content: string, options?: IOptions) {
-  const c = content.split("\n");
-  const res: string[] = [];
-
-  for (const l of c) {
-    if (l !== "") {
-      res.push(l);
-    } else if (res.length === 0 || res.at(-1) !== "") {
-      res.push(l);
-    }
-  }
-
-  return res.join("\n");
+export function formatThrift(content: string, options?: IOptions): string {
+  const t = new ThriftFormatter(options);
+  return t.format(content);
 }
---
+++ src/internal/ThriftFormatter.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { IOptions } from "../types";
+
+class Options implements IOptions {
+  indent = 4;
+}
+
+class StructLine {
+  number = "";
+  paramOptions = "";
+  paramName = "";
+  defaultValue = "";
+  description = "";
+  annotation = "";
+}
+
+class EnumLine {
+  number = "";
+  paramName = "";
+  hasDescription = "";
+  description = "";
+  annotation = "";
+}
+
+class ThriftFormatter {
+  options: IOptions;
+
+  constructor(options?: IOptions) {
+    this.options = options ?? new Options();
+  }
+
+  public format(content: string): string {
+    content = this.deleteExtraEmptyLines(content);
+    return content;
+  }
+
+  deleteExtraEmptyLines(content: string) {
+    const c = content.split("\n");
+    const res: string[] = [];
+
+    for (const l of c) {
+      if (l !== "") {
+        res.push(l);
+      } else if (res.length === 0 || res.at(-1) !== "") {
+        res.push(l);
+      }
+    }
+
+    return res.join("\n");
+  }
+}
+
+export { ThriftFormatter };
+export default ThriftFormatter;
