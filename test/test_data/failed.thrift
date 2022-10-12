d84be9abe89de0e0032f97604e39e6733cfb73b7
Dup4 <lyuzhi.pan@gmail.com>
2022-10-12 16:22:37 +0800

    test: add more case

---
+++ test/test_data/failed.thrift
@@ -0,0 +1,16 @@
+namespace cpp idl.c
+
+// a
+// b
+
+  struct A {
+    1: required /* ddd */ string a,
+    2: required i32 b,
+    3: optional i64 c,
+}
+
+
+
+struct B {
+    1: required string b,
+}
