945e2464e2af805db5e89ff403fcdc115016fee9
Dup4 <lyuzhi.pan@gmail.com>
2022-10-15 11:24:34 +0800

    test: add TailComments case

---
+++ test/test_data/TailComments.thrift
@@ -0,0 +1,9 @@
+// ServiceLineComment
+
+service ApiTag {
+   void Bar() (api.post="/Bar", api.data_policy = "ENABLE", api.serializer = "json", api.resp_serializer= "json"), // Bar Comment
+    void Baz() (api.post="/Baz", api.data_policy = "ENABLE", api.serializer = "json", api.resp_serializer= "json"), // Baz Comment
+    void Foo() (api.post="/Foo", api.data_policy = "ENABLE", api.serializer = "json", api.resp_serializer= "json"), // Foo
+
+    void Deprecated() (deprecated), // Deprecated
+}
